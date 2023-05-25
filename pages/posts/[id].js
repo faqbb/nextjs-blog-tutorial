9917c354205188c5b64710e3dafd5bb9a7f07ca6
Facu <84508942+faqbb@users.noreply.github.com>
2023-05-24 21:50:51 -0300

    frontend-blog-tutorial

---
+++ pages/posts/[id].js
@@ -0,0 +1,38 @@
+import Head from "next/head.js";
+import Layout from "../../components/layouts.js";
+import { getAllPostIds, getPostData } from "../../lib/posts.js";
+import Date from "../../components/date.js";
+import utilStyles from '../../styles/utils.module.css';
+
+
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false,
+    }
+}
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id);
+    return {
+        props: {
+            postData,
+        }
+    }
+}
+export default function Post ({ postData }) {
+    return <Layout>
+                <Head>
+                    <title>{postData.title}</title>
+                </Head>
+                <article>
+                    <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                    <div className={utilStyles.lightText}>
+                        <Date dateString={postData.date} />
+                    </div>
+                    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+                </article>
+            </Layout>
+}
+
